2ccbfcc5e9846e4b12a66f983d08d9008e5fd4d6
Guillaume Villerez <gvi@zenexity.com>
2016-11-24 14:09:09 +0100

    Pixelizer

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,32 @@
 "use strict";
 
-var express = require('express');
-var sharp = require('sharp');
-var fs = require('fs');
-var path = require('path');
-var router = express.Router();
+const express = require('express');
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+const uuid = require('uuid');
 
+var pixeliser = function (dir) {
+    return (req, res, next) => {
+        let blurLevel = parseInt(req.params.blurLevel);
 
+        let transformerDown = sharp().resize(blurLevel);
+        let transformerUp = sharp().resize(800, undefined, { interpolator : 'nearest' });
+
+        req.outputFile = fs.createReadStream(path.join(dir, req.params.fileName))
+            .pipe(transformerDown)
+            .pipe(transformerUp);
+
+        next();
+    }
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    let transformerDown = sharp().resize(20, 20);
-    let transformerUp = sharp().resize(600, 600, { interpolator : 'nearest' });
-
-    fs.createReadStream('trump.jpg')
-        .pipe(transformerDown)
-        .pipe(transformerUp)
-        .pipe(res);
+router.get('/:blurLevel/:fileName', pixeliser(__dirname + "/../"), function(req, res, next) {
+    console.log("blurLevel: " + req.params.blurLevel);
+    console.log("fileName: " + req.params.fileName);
+    req.outputFile.pipe(res);
 });
 
 module.exports = router;
