34d10ce6f2addc376847c12000fc77a2e6d0766d
Egor BEREZOVSKIY <ebe@zengularity.com>
2016-11-24 14:10:01 +0100

    Add facebook sdk

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var fb = require('fb');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET users listing. */
+router.get('/profile', function(req, res, next) {
   res.send('respond with a resource');
 });
 
